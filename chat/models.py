0854e9c332171a5bf334d1329fa9f30a4c0184b1
Ilya2106 <96929335+Ilya2106@users.noreply.github.com>
2022-12-05 09:38:40 +0300

    Update models.py

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from accounts.models import CustomUser
 
-# Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    online = models.ManyToManyField(to=CustomUser, blank=True)
+
+    def get_online_count(self):
+        return self.online.count()
+
+    def join(self, user):
+        self.online.add(user)
+        self.save()
+
+    def leave(self, user):
+        self.online.remove(user)
+        self.save()
+
+    def __str__(self):
+        return f"{self.name} ({self.get_online_count()})"
+
+
+class Message(models.Model):
+    user = models.ForeignKey(to=CustomUser, on_delete=models.CASCADE)
+    room = models.ForeignKey(to=Room, on_delete=models.CASCADE)
+    content = models.CharField(max_length=512)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}: {self.content} [{self.timestamp}]"
